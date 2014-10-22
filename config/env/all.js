a42217d719f743d285191d327924c0da9ad97a03
unknown <vbogdanets@VBogdanets.kiev.luxoft.com>
2014-10-22 17:05:53 +0300

    initial

--- config/env/all.js
+++ config/env/all.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 	app: {
-		title: 'MEAN.JS',
-		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport'
+		title: 'BEX.Luxoft',
+		description: '',
+		keywords: ''
 	},
 	port: process.env.PORT || 3000,
 	templateEngine: 'swig',
-	sessionSecret: 'MEAN',
+	sessionSecret: 'BEX',
 	sessionCollection: 'sessions',
 	assets: {
 		lib: {
